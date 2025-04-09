09f4b2d1ffdab8bec0c85a9ab4c63af0363c1965
Avi <avi6106@gmail.com>
2025-04-09 22:14:25 +0300

    Added a player controls script for DataCamp

---
+++ src/player-controls-for-datacamp.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Player controls for DataCamp
+// @namespace    http://tampermonkey.net/
+// @version      0.0
+// @description
+// @author       Avi (https://avi12.com)
+// @copyright    2025 Avi (https://avi12.com)
+// @license      MIT
+// @match        https://projector.datacamp.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=datacamp.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  document.addEventListener("keydown", e => {
+    const elPlaybackRateCurrent = document.querySelector(".vjs-menu-item[aria-checked=true]");
+    const elVideo = document.querySelector("video");
+
+    switch (e.code) {
+      case "Home":
+        elVideo.currentTime = 0;
+        break;
+
+      case "End":
+        elVideo.currentTime = elVideo.duration;
+        break;
+
+      case "Comma":
+        if (!e.shiftKey) {
+          return;
+        }
+        elPlaybackRateCurrent.nextElementSibling?.click();
+        elVideo.focus();
+        break;
+
+      case "Period":
+        if (!e.shiftKey) {
+          return;
+        }
+        elPlaybackRateCurrent.previousElementSibling?.click();
+        elVideo.focus();
+        break;
+    }
+  });
+})();
