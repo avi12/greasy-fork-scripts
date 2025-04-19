df36a4d5531ddbb6c457aad3778324c6fadf1bae
Avi <avi6106@gmail.com>
2025-04-19 19:28:27 +0300

    Update Twitch dark mode logic for simplicity and efficiency

--- src/auto-dark-mode-twitch.js
+++ src/auto-dark-mode-twitch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Dark Mode for Twitch
 // @namespace    https://bengrant.dev
-// @version      0.1
+// @version      0.2
 // @description  Works for desktop
 // @author       Avi (https://avi12.com)
 // @copyright    2025 Avi (https://avi12.com)
@@ -16,31 +16,11 @@
 
   const darkQuery = matchMedia("(prefers-color-scheme: dark)");
 
-  function isWebPageDark() {
-    return localStorage.getItem("twilight.theme") === "1";
+  function setDark(isDark) {
+    document.body.classList.value = document.body.classList.value.replace(/tw-root--theme-(light|dark)/, isDark ? "tw-root--theme-dark" : "tw-root--theme-light");
+    localStorage.setItem("twilight.theme", `${isDark ? 1 : 0}`);
   }
 
-  const setIsDark = isDark => {
-    if (isWebPageDark() && isDark || !isWebPageDark() && !isDark) {
-      return;
-    }
-
-    const getElDarkModeToggle = () => document.querySelector("[data-a-target=dark-mode-toggle]");
-    const elProfilePicture = document.querySelector("[data-a-target=user-menu-toggle]");
-    const {activeElement} = document;
-    if (!getElDarkModeToggle()) {
-      elProfilePicture.click();
-    }
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        getElDarkModeToggle().parentElement.querySelector("label").click();
-        elProfilePicture?.click();
-        activeElement.focus();
-      });
-    });
-  }
-
-
-  setIsDark(darkQuery.matches);
-  darkQuery.addEventListener("change", e => setIsDark(e.matches));
+  setDark(darkQuery.matches);
+  darkQuery.addEventListener("change", e => setDark(e.matches));
 })();
