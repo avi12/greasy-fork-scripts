0e650136caf57ce934da68fb54b3666ed20b2bb9
Avi <avi6106@gmail.com>
2025-05-23 14:00:07 +0300

    auto-dark-mode-reddit.js: Fixed script

--- src/auto-dark-mode-reddit.js
+++ src/auto-dark-mode-reddit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Dark Mode for Reddit
 // @namespace    https://bengrant.dev
-// @version      0.2
+// @version      0.3
 // @description  Works for desktop
 // @author       Avi (https://avi12.com)
 // @copyright    2025 Avi (https://avi12.com)
@@ -15,14 +15,70 @@
   "use strict";
 
   /**
-   * @param theme {"dark" | "light"}
+   * @returns {HTMLButtonElement}
    */
-  function setTheme(theme) {
-    document.body.classList.value = document.body.classList.value.replace(/theme-(dark|light)/, `theme-${theme}`);
-    document.cookie = `theme=${theme === "dark" ? 2 : 1};`;
+  const getElButtonProfile = () => document.querySelector("button#expand-user-drawer-button");
+
+  // noinspection CssInvalidHtmlTagReference
+  /**
+   * @returns {HTMLElement}
+   */
+  const getElDarkToggle = () => document.querySelector("#darkmode-list-item faceplate-switch-input");
+
+  const OBSERVER_OPTIONS = {childList: true, subtree: true};
+
+  function openProfileDrawerAndToggleTheme() {
+    const {activeElement} = document;
+    const elButtonProfile = getElButtonProfile();
+    new MutationObserver((_, observer) => {
+      const elDarkToggle = getElDarkToggle();
+      if (!elDarkToggle) {
+        return;
+      }
+      const {shadowRoot} = elDarkToggle;
+      if (!shadowRoot) {
+        return;
+      }
+      const elDarkToggleInput = shadowRoot.querySelector("input");
+      if (!elDarkToggleInput) {
+        return;
+      }
+      observer.disconnect();
+      requestAnimationFrame(() => {
+        elDarkToggle.click();
+        elButtonProfile.click();
+        activeElement.focus();
+      });
+    }).observe(document, OBSERVER_OPTIONS);
+    elButtonProfile.click();
   }
 
   const darkQuery = matchMedia("(prefers-color-scheme: dark)");
-  setTheme(darkQuery.matches ? "dark" : "light");
-  darkQuery.addEventListener("change", e => setTheme(e.matches ? "dark" : "light"));
+  new MutationObserver((_, observer) => {
+    if (!getElButtonProfile()) {
+      return;
+    }
+
+    const themeNew = darkQuery.matches ? "dark" : "light";
+    const themeCurrent = document.cookie.match(/theme=([12])/)[1] === "1" ? "light" : "dark";
+    const isChangeTheme = themeNew !== themeCurrent;
+    observer.disconnect();
+    if (!isChangeTheme) {
+      return;
+    }
+
+    openProfileDrawerAndToggleTheme();
+  }).observe(document, OBSERVER_OPTIONS);
+
+  darkQuery.addEventListener("change", () => {
+    const elDarkToggle = getElDarkToggle();
+    if (elDarkToggle) {
+      const {activeElement} = document;
+      elDarkToggle.click();
+      activeElement.focus();
+      return;
+    }
+
+    openProfileDrawerAndToggleTheme();
+  });
 })();
